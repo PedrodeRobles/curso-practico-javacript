24f7046666cd66a903ef6f5be09f08f061e2aa5a
Pedro de Robles <piterddr@gmail.com>
2021-07-12 23:50:02 -0300

    Agregue la funcionalidad de hacer la operacion con cupones, en vez de descuentos escritos manualmente

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,12 @@
 // const precioOriginal = 120;
 // const descuento = 18;
 
+const coupons = [
+    "cupon1",
+    "cupon2",
+    "cupon3",
+]
+
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioDescuento) / 100;
@@ -12,16 +18,38 @@ function calcularPrecioConDescuento(precio, descuento) {
 
 function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
-    const inputDiscount = document.getElementById("InputDiscount");
-
     const valuePrice = inputPrice.value;
-    const valueDiscount = inputDiscount.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(valuePrice, valueDiscount);
+    // const inputDiscount = document.getElementById("InputDiscount");
+    // const valueDiscount = inputDiscount.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const valueCoupon = inputCoupon.value;
+
+    let descuento;
+
+    switch(valueCoupon) {
+        case "cupon1":
+            descuento = 15;
+        break;    
+        case "cupon2":
+            descuento = 30;
+        break;    
+        case "cupon3":
+            descuento = 25;    
+        break;    
+    }
+    
+    
+    const precioConDescuento = calcularPrecioConDescuento(valuePrice, descuento);
 
     const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    resultP.innerText = "El descuento es de " + descuento + "% y el precio con descuento son: $" + precioConDescuento;
 }
+
+
+
+
 
 
 
