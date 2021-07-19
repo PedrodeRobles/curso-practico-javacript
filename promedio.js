7440d78c569a3316009d261b8b588437b4b503e5
Pedro de Robles <piterddr@gmail.com>
2021-07-18 21:21:05 -0300

    Agregue etiquetas en html y las uni al promedio.js para que apareza en el navegador

--- promedio.js
+++ promedio.js
@@ -21,3 +21,23 @@ function calcularMediaAritmetica(Lista) { //calcularMediaAritmetica([1, 14 ,151,
 
     return promedio;
 }
+
+
+// HTML
+function resultado() {
+    const input1 = document.getElementById("Input1");
+    const value1 = Number(input1.value);
+
+    const input2 = document.getElementById("Input2");
+    const value2 = Number(input2.value);
+
+    const input3 = document.getElementById("Input3");
+    const value3 = Number(input3.value);
+
+    const listaUsuario = [value1, value2, value3,];
+
+    const resultado = calcularMediaAritmetica(listaUsuario);
+    
+    const result = document.getElementById("Result");
+    result.innerText = "El promedio es: " + Number.parseFloat(resultado).toFixed(2);;
+}
