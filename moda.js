63f6e511d523d34a8310b1f80cd7b7aafca28be8
Pedro de Robles <piterddr@gmail.com>
2021-07-18 22:03:37 -0300

    Agregue interaccion desde el navegador

--- moda.js
+++ moda.js
@@ -57,3 +57,34 @@ function calcularModa(lista) {
     return moda;
 
 }
+
+//HTML
+function calcularInputs() {
+    const input1 = document.getElementById("Input1")
+    const value1 = input1.value;
+
+    const input2 = document.getElementById("Input2")
+    const value2 = input2.value;
+
+    const input3 = document.getElementById("Input3")
+    const value3 = input3.value;
+
+    const input4 = document.getElementById("Input4")
+    const value4 = input4.value;
+
+    const input5 = document.getElementById("Input5")
+    const value5 = input5.value;
+
+    const input6 = document.getElementById("Input6")
+    const value6 = input6.value;
+
+    const input7 = document.getElementById("Input7")
+    const value7 = input7.value;
+
+    const listaUsuario = [value1, value2, value3, value4, value5, value6, value7];
+
+    const resultado = calcularModa(listaUsuario);
+
+    const result = document.getElementById("Result");
+    result.innerText = "La moda es: " + resultado[0] + " se repite " + resultado[1] + " veces";
+}
